3506be35ab35deba144153ccb4345c67a816e734
Isaia-mhd <mohamedesaie21@gmail.com>
2025-09-10 11:06:49 +0300

    fix: move the services icons to the service folder for clarifying

--- app/Services/ServicesComponent.php
+++ app/Services/ServicesComponent.php
@@ -8,22 +8,22 @@ class ServicesComponent{
             [
                 "service" => "Développement Web",
                 "description" => "Sites et applis web performants et sécurisés.",
-                "icon" => "/images/service_devweb.png"
+                "icon" => "/images/services/service_devweb.png"
             ],
             [
                 "service" => "Développement Mobile",
                 "description" => "Applis mobiles intuitives iOS et Android.",
-                "icon" => "/images/service_devmobile.png"
+                "icon" => "/images/services/service_devmobile.png"
             ],
             [
                 "service" => "UI/UX Design",
                 "description" => "Interfaces ergonomiques et attrayantes.",
-                "icon" => "/images/service_design.png"
+                "icon" => "/images/services/service_design.png"
             ],
             [
                 "service" => "Administration réseau et système",
                 "description" => "Gestion et sécurité IT fiables.",
-                "icon" => "/images/service_reseau.png"
+                "icon" => "/images/services/service_reseau.png"
             ]
         ];
     }
