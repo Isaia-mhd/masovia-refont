c68edf71e810e65572bb854bc5919ad56a7aa7fd
Isaia-mhd <mohamedesaie21@gmail.com>
2025-09-12 14:16:26 +0300

    fix: add border dashed on tarif component

--- resources/views/components/tarif_card.blade.php
+++ resources/views/components/tarif_card.blade.php
@@ -1,14 +1,17 @@
-<div class="w-[100%] text-white bg-[#585980] rounded-md py-6 cursor-pointer space-y-10 px-6 hover:bg-[#F0F235] hover:text-black group transition duration-175">
+<div
+    class="w-[100%] text-white bg-[#585980] rounded-md py-6 cursor-pointer space-y-10 px-6 hover:bg-[#F0F235] hover:text-black group transition duration-175">
     <div class="h-[50px] text-center">
         <p class="text-2xl text-start font-semibold">{{ $tarif['category'] }}</p>
         <h3 class=" font-normal text-xs text-start">{{ $tarif['description'] }}</h3>
+        <p class="border-dashed border-1 border-gray-500 mt-3"></p>
     </div>
     <div class="w-full">
         <p class="text-2xl font-semibold text-start">
             € {{ $tarif['type']['month'] }} / mois
         </p>
     </div>
-    <div class="w-full text-center bg-[#585980] border-1 border-gray-300 rounded-md py-3 group-hover:!bg-black group-hover:text-white group-hover:border-none">
+    <div
+        class="w-full text-center bg-[#585980] border-1 border-gray-300 rounded-md py-3 group-hover:!bg-black group-hover:text-white group-hover:border-none">
         <a href="" class="w-full py-3 px-[10%] ">Planifier une démo</a>
     </div>
 
